6317fa03ee2d8e3ee9cf56c30330bc1e0d3f596b
nick L <xnel417x@gmail.com>
2020-07-04 18:50:59 -0500

    Updates

---
+++ precache-manifest.0cdcca608bcd16bfcd85846d6eb7681e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "96653d79910278f5c913e3297637a01b",
+    "url": "/news-site/index.html"
+  },
+  {
+    "revision": "20073a72050f2f1aa3cc",
+    "url": "/news-site/static/js/2.d81defdf.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/news-site/static/js/2.d81defdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b61b82c1292486a23fb9",
+    "url": "/news-site/static/js/main.254c4425.chunk.js"
+  },
+  {
+    "revision": "a4466e90513ddb7fd5a1",
+    "url": "/news-site/static/js/runtime-main.b3235af8.js"
+  }
+]);
